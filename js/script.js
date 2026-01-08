6c47b0b455a56d1ba7d4c7f1bc653b6f9c54f99f
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-01-08 15:42:02 +0100

    Inizio progettazione js, creato l'endpoint

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Riferimento endpoint
+const endPoint = "https://lanciweb.github.io/demo/api/pictures/";
+
+// Elemento di outputHtml
+const containerOutput = document.querySelector("container");
+
